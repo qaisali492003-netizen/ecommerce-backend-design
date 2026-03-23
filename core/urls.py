bb958e0a90f0b25d537c9558b383c5b7c08232ea
qaisali492003-netizen <qais.ali492003@gmail.com>
2026-03-23 18:41:22 +0500

    Emergency fix for product images

--- core/urls.py
+++ core/urls.py
@@ -2,16 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import redirect # 1. Import redirect
+from django.shortcuts import redirect
+from django.views.static import serve # Add this import
+import re # Add this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('shop.urls')),
-    
-    # 2. Redirect the root URL ('') to your products page
     path('', lambda request: redirect('products/', permanent=True)),
+    
+    # This force-serves media files even when DEBUG is False
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# This adds the media path to the existing list
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
